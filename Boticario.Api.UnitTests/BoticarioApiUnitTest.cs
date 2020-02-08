d53a4c7c55cfec1deb806d19ea40800fedcfc2ff
JHONATAN TABORDA (CRGL-THIRDPARTY.COM) <jhonatan_taborda@crgl-thirdparty.com>
2020-02-07 23:19:08 -0300

    inclusão do projeto de test

---
+++ Boticario.Api.UnitTests/BoticarioApiUnitTest.cs
@@ -0,0 +1,25 @@
+using Boticario.Api.Controllers;
+using Boticario.Api.Repository;
+using Boticario.Api.Repository.Interfaces;
+using System;
+using Xunit;
+
+namespace Boticario.Api.UnitTests
+{   
+    public class BoticarioApiUnitTest
+    {
+        UserController _userController;
+        IUserRepository _repository;
+
+        public BoticarioApiUnitTest()
+        {
+            
+        }
+
+        [Fact]
+        public void GetAuthToken()
+        {
+
+        }
+    }
+}
